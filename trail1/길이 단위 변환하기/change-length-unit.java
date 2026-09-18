1a81abe74ccf0508871cea1e567f373ad1a2fe0d
kjs31602 <kjs31602@gmail.com>
2026-09-18 21:31:36 +0900

    [Passed] 길이 단위 변환하기 (95ms, 9MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double ft = 30.48;
+        double mi = 160934;
+        double a = 9.2;
+        double b = 1.3;
+        System.out.printf("%.1fft = %.1fcm\n", a, a*ft);
+        System.out.printf("%.1fmi = %.1fcm", b, b*mi);
+    }
+}
